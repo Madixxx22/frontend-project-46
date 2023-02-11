fe1e0991424b2e45355790b62b3e8ad9188b8fdb
Madixxx22 <vysokolyan2016@gmail.com>
2023-02-11 23:49:51 +0300

    added gendiff -h more info

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,6 +6,9 @@ const program = new Command();
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('0.8.0')
+  .version('0.0.1')
+  .arguments('filepath1')
+  .arguments('filepath2')
+  .option('-f --format <type>', 'output format')
   
 program.parse();
