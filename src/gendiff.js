4e90d4cdf23a38ad1a4d24f3fca605f03f0f0f66
Madixxx22 <vysokolyan2016@gmail.com>
2023-02-13 16:01:44 +0300

    fix

--- src/gendiff.js
+++ src/gendiff.js
@@ -3,13 +3,16 @@ import path from 'path';
 import { getParsingData } from './parsers.js';
 import { engineDiff, formatting } from './generate-difference.js';
 
-const getData = (filepath) => fs.readFileSync(path.resolve(process.cwd(), filepath), 'utf8'); 
+const getData = (filepath) => fs.readFileSync(path.resolve(process.cwd(), filepath), 'utf8');
 
 /* eslint-disable-next-line */
 export const genDiff = (filepath1, filepath2, format) => {
   try {
     const firstFile = getParsingData(getData(filepath1), filepath1);
     const secondFile = getParsingData(getData(filepath2), filepath2);
+    if (firstFile === 'error' || secondFile === 'error') {
+      return 'Неверный формат файлов';
+    }
     const difference = engineDiff(firstFile, secondFile);
     return formatting(difference);
   } catch (err) {
--- src/parsers.js
+++ src/parsers.js
@@ -1,14 +1,15 @@
-import yaml from 'js-yaml'
+import yaml from 'js-yaml';
 
-const getFormat = (path) => {
-    return path.split('.')[1]
-}
+const getFormat = (path) => path.split('.')[1];
 
 export const getParsingData = (data, path) => {
-    const format = getFormat(path);
-    if (format === 'json'){
-        return JSON.parse(data);
-    } else if (format === 'yml' || 'yaml') {
-        return yaml.load(data);
-    }
-}
+  const format = getFormat(path);
+  if (format === 'json') {
+    return JSON.parse(data);
+  } if (format === 'yml' || format === 'yaml') {
+    return yaml.load(data);
+  }
+  return 'error';
+};
+
+export default getParsingData;
