4fe698a2ebd2d13fa1b2ab0638811e1ff23ad911
Madixxx22 <vysokolyan2016@gmail.com>
2023-02-15 00:35:16 +0300

    fix

--- src/formatter/formatter.js
+++ src/formatter/formatter.js
@@ -1,12 +1,11 @@
 import { stylish } from './stylish.js';
 import { plain } from './plain.js';
 
-
 /* eslint-disable-next-line */
 export const formatting = (diff, format) => {
   if (format === 'stylish') {
     return stylish(diff);
-  } else if (format === 'plain') {
+  } if (format === 'plain') {
     return plain(diff);
   }
 };
