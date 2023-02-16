b8cdde9a6521b529418769b376c1e9d0f565ae12
Madixxx22 <vysokolyan2016@gmail.com>
2023-02-16 20:53:55 +0300

    change main in package.json and refactoring engineDiff

--- src/generate-difference.js
+++ src/generate-difference.js
@@ -6,41 +6,21 @@ export const engineDiff = (obj1, obj2) => {
   ).sort();
   const diff = mergeKeys.map((key) => {
     if (!_.has(obj1, key)) {
-      return {
-        key,
-        new: obj2[key],
-        status: 'added',
-      };
+      return { key, new: obj2[key], status: 'added' };
     } if (!_.has(obj2, key)) {
-      return {
-        key,
-        old: obj1[key],
-        status: 'removed',
-      };
+      return { key, old: obj1[key], status: 'removed' };
     } if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
       const child = engineDiff(obj1[key], obj2[key]);
-      return {
-        key,
-        children: child,
-        status: 'nested',
-      };
+      return { key, children: child, status: 'nested' };
     } if (obj1[key] === obj2[key]) {
-      return {
-        key,
-        value: obj1[key],
-        status: 'unchanged',
-      };
+      return { key, value: obj1[key], status: 'unchanged' };
     } if (obj1[key] !== obj2[key]) {
       return {
-        key,
-        old: obj1[key],
-        new: obj2[key],
-        status: 'changed',
+        key, old: obj1[key], new: obj2[key], status: 'changed',
       };
     }
     return '';
   });
-
   return diff;
 };
 
